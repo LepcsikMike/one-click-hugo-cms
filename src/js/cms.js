321a533100a884f9102a01e2ed9d9559c787eda1
mike <Mike.Lepcsik@cib.de>
2020-07-09 16:33:19 +0200

    add values

--- src/js/cms.js
+++ src/js/cms.js
@@ -8,6 +8,7 @@ import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
 import ProductsPreview from "./cms-preview-templates/products";
 import ValuesPreview from "./cms-preview-templates/values";
+import TestValuesPreview from "./cms-preview-templates/testvalues";
 import ContactPreview from "./cms-preview-templates/contact";
 
 CMS.registerPreviewStyle(styles, { raw: true });
@@ -15,5 +16,6 @@ CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("products", ProductsPreview);
 CMS.registerPreviewTemplate("values", ValuesPreview);
+CMS.registerPreviewTemplate("testvalues", TestValuesPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
 CMS.init();
